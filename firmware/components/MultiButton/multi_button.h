d0e9f1ecc99a136689203695f135e6db69dfe51f
TDA-2030 <1932489836@qq.com>
2020-09-26 21:26:06 +0800

    fix(MultiButton): modify LONG_TICKS to 10 second

--- firmware/components/MultiButton/multi_button.h
+++ firmware/components/MultiButton/multi_button.h
@@ -13,7 +13,7 @@
 #define TICKS_INTERVAL    5	//ms
 #define DEBOUNCE_TICKS    3	//MAX 8
 #define SHORT_TICKS       (300 /TICKS_INTERVAL)
-#define LONG_TICKS        (1000 /TICKS_INTERVAL)
+#define LONG_TICKS        (10000 /TICKS_INTERVAL)
 
 
 typedef void (*BtnCallback)(void*);
